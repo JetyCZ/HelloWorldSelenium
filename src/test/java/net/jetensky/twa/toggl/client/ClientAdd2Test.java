5ce66a3b51b25fdd3bc8041c3bc63014b5f9cda3
Pavel Jetensky <pavel.jetensky@hooyu.com>
2017-10-22 23:34:17 +0200

    Cleaning up unnecessary files

--- src/test/java/net/jetensky/twa/toggl/client/ClientAdd2Test.java
+++ src/test/java/net/jetensky/twa/toggl/client/ClientAdd2Test.java
@@ -1,7 +1,6 @@
 package net.jetensky.twa.toggl.client;
 
 import net.jetensky.twa.JUnitTestBase;
-import net.jetensky.twa.pages.IndexPage;
 import net.jetensky.twa.util.TestUtil;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ClientAdd2Test extends JUnitTestBase {
 
 
-  @Autowired IndexPage indexPage;
   @Autowired TestUtil testUtil;
 
   @Test
--- src/test/java/net/jetensky/twa/toggl/smoke/LogInTest.java
+++ src/test/java/net/jetensky/twa/toggl/smoke/LogInTest.java
@@ -14,5 +14,4 @@ public class LogInTest extends JUnitTestBase {
     testUtil.loginTestUser();
   }
 
-
 }
